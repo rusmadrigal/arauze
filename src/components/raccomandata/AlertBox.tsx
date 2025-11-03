1044350ee1dba8f22a1db4b15c7aa9ef68de5733
Rus <rusbenmadrigal@gmail.com>
2025-11-02 20:36:46 -0600

    Alertbox update

--- src/components/raccomandata/AlertBox.tsx
+++ src/components/raccomandata/AlertBox.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function AlertBox() {
   return (
     <section className="mt-10">
-      <div className="w-full rounded-2xl bg-gradient-to-r from-[#3A78E0] to-[#2F66D5] text-white px-6 md:px-8 py-7 shadow-card">
+      <div className="w-full rounded-2xl bg-linear-to-r from-[#3A78E0] to-[#2F66D5] text-white px-6 md:px-8 py-7 shadow-card">
         <h3 className="text-lg font-semibold mb-2">
           Attenzione ai Termini di Ritiro
         </h3>
