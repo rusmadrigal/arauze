4c8d954fd501c11be0bff6668dc546c533e5c6fc
Rus Madrigal <rusbenmadrigal@gmail.com>
2025-11-13 23:07:52 -0600

    Home update

--- src/components/home/TextBlockHome.tsx
+++ src/components/home/TextBlockHome.tsx
@@ -3,7 +3,7 @@ export default function TextBlockHome() {
   return (
     <section className="mt-12">
       <h3 className="text-2xl font-semibold mb-2">
-        Che cos’è una raccomandata market, AR o 697?
+        Che cos’è una Raccomandata Market?
       </h3>
       <p className="text-gray-600 leading-relaxed">
         Una raccomandata market, AR o con codice 697 è un tipo di comunicazione
