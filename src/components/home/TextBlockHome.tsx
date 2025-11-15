f478c6ecf81d793231705e0d382d507157acee19
Rus Madrigal <rusbenmadrigal@gmail.com>
2025-11-14 20:28:23 -0600

    Home text update

--- src/components/home/TextBlockHome.tsx
+++ src/components/home/TextBlockHome.tsx
@@ -1,15 +1,43 @@
 // src/components/home/TextBlockHome.tsx
 export default function TextBlockHome() {
   return (
-    <section className="mt-12">
-      <h3 className="text-2xl font-semibold mb-2">
+    <section className="mt-12 space-y-4">
+      <h3 className="text-2xl font-semibold mb-4">
         Che cos’è una Raccomandata Market?
       </h3>
+
       <p className="text-gray-600 leading-relaxed">
-        Una raccomandata market, AR o con codice 697 è un tipo di comunicazione
-        inviata tramite Poste Italiane che indica generalmente documenti ufficiali
-        o notifiche da enti pubblici o privati. Il codice permette di identificare
-        la tipologia della raccomandata e seguirne lo stato di consegna.
+        Raccomandata Market è il sistema che comprende aziende, enti pubblici e
+        fornitori di servizi che inviano lettere raccomandate per comunicare in
+        modo ufficiale con i destinatari (clienti). A differenza della posta
+        tradizionale, le notifiche inviate tramite Raccomandata Market hanno
+        valore legale.
+      </p>
+
+      <p className="text-gray-600 leading-relaxed">
+        Raccomandata Market registra ogni fase della spedizione e viene
+        solitamente utilizzato per comunicazioni importanti quali: multe,
+        pagamenti in ritardo, operazioni bancarie, contratti, assicurazioni,
+        procedimenti giudiziari, comunicazioni amministrative, tra le altre.
+      </p>
+
+      <p className="text-gray-600 leading-relaxed">
+        È importante sottolineare che, sebbene la corrispondenza inviata tramite
+        Raccomandata Market sia di natura legale, non tutte le notifiche
+        riguardano addebiti, citazioni in giudizio o tributi. In molti casi le
+        banche utilizzano questo mezzo per inviare estratti conto, sostituzioni
+        di carte di credito o semplicemente informazioni generali a un
+        determinato gruppo di clienti.
+      </p>
+
+      <p className="text-gray-600 leading-relaxed">
+        Noi di <strong>Arauze.com</strong> ci siamo assunti il compito di
+        iniziare a raccogliere le informazioni pubbliche relative ai diversi
+        codici di Raccomandata Market, in modo che voi, in qualità di utenti,
+        possiate avere un&apos;idea più chiara di ciò che potrebbe contenere
+        l&apos;avviso di ritiro. Allo stesso modo, potete utilizzare Arauze.com
+        per segnalare il contenuto della vostra notifica e aiutare così altri
+        utenti.
       </p>
     </section>
   );
