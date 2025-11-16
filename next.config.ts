afd1addf2568aac826b7675163adb338e985f40c
Rus Madrigal <rusbenmadrigal@gmail.com>
2025-11-15 19:19:59 -0600

    Sanity CDN

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
