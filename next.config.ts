fdee9933ec742dc02705fde5e9023776af96426b
Rus Madrigal <rusbenmadrigal@gmail.com>
2025-11-28 22:31:44 -0600

    Redirects

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,64 @@ const nextConfig: NextConfig = {
   },
 
   // -------------------------
+  // 🔁 Redirects SEO
+  // -------------------------
+  async redirects() {
+    return [
+      {
+        source: "/raccomandata/bologna-cmp",
+        destination: "/raccomandata/cmp/bologna",
+        permanent: true,
+      },
+      {
+        source: "/raccomandata/catania-cmp",
+        destination: "/raccomandata/cmp/catania",
+        permanent: true,
+      },
+      {
+        source: "/raccomandata/cmp-borromeo",
+        destination: "/raccomandata/cmp/borromeo",
+        permanent: true,
+      },
+      {
+        source: "/raccomandata/cmp-cagliari",
+        destination: "/raccomandata/cmp/cagliari",
+        permanent: true,
+      },
+      {
+        source: "/raccomandata/cmp-verona",
+        destination: "/raccomandata/cmp/verona",
+        permanent: true,
+      },
+      {
+        source: "/raccomandata/market",
+        destination: "/raccomandata-market",
+        permanent: true,
+      },
+      {
+        source: "/raccomandata/milano-roserio-cmp",
+        destination: "/raccomandata/cmp/roserio",
+        permanent: true,
+      },
+      {
+        source: "/raccomandata/napoli-cmp",
+        destination: "/raccomandata/cmp/napoli",
+        permanent: true,
+      },
+      {
+        source: "/raccomandata/roma-cmp",
+        destination: "/raccomandata/cmp/roma",
+        permanent: true,
+      },
+      {
+        source: "/raccomandata/torino-cmp",
+        destination: "/raccomandata/cmp/torino",
+        permanent: true,
+      },
+    ];
+  },
+
+  // -------------------------
   //  No index staging
   // -------------------------
   async headers() {
