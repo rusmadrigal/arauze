c33198bfad70ffb6b11f7ba9739ef1f8fba52e7b
Rus Madrigal <rusbenmadrigal@gmail.com>
2026-02-01 22:12:04 -0600

    Update comment in HeroHeader component for clarity

--- src/components/home/HeroHeader.tsx
+++ src/components/home/HeroHeader.tsx
@@ -6,7 +6,7 @@ export default function HeroHeader() {
     <header className="rounded-2xl shadow-card bg-white p-6 md:p-10 pt-12 md:pt-16 pb-5">
       <section className="grid grid-cols-1 md:grid-cols-2 items-center gap-8 md:gap-12">
         
-        {/* Texto principal */}
+        {/* Texto principal de la web*/}
 
         <div className="w-full md:pr-2 max-w-3xl">
           <h1
